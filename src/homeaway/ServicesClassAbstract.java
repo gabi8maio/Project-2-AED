f8068b0591aaa6afed905bc2c6396c13d59ea907
gabi8maio <mikeby4k@gmail.com>
2025-11-29 10:53:23 +0000

    A funcionar ate ao 10

--- src/homeaway/ServicesClassAbstract.java
+++ src/homeaway/ServicesClassAbstract.java
@@ -119,6 +119,10 @@ public abstract class ServicesClassAbstract implements Services, ServicesChange{
     public void setNumOfInsertion(int counter){
         numOfServiceInsertion = counter;
     }
+
+    public int getNumOfInsertion(){
+        return numOfServiceInsertion;
+    }
     @Override
     public void addRating(int stars,String tag, int counter) {
         int oldAverage = averageStars;
